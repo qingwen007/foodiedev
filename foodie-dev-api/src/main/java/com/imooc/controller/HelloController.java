d0944a7e794699c0464b38d4d3af90964795d825
qingwen007 <1030679732@qq.com>
2020-12-30 00:33:55 +0800

    log4j

--- foodie-dev-api/src/main/java/com/imooc/controller/HelloController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/HelloController.java
@@ -1,13 +1,22 @@
 package com.imooc.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
+
+@ApiIgnore
 @RestController
 public class HelloController {
+     final static Logger logger= LoggerFactory.getLogger(HelloController.class);
     @GetMapping("/hello")
     public Object hello()
     {
+        logger.info("info: Hello");
+        logger.warn("warn: Hello");
+        logger.error("error: Hello");
         return "hello";
     }
 }
